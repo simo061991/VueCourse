fe7c9837cf8c98ef99b5e791c6bffd5bbc87f089
unknown <simo.matijevic@gmail.com>
2023-09-05 20:26:55 +0200

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header>
+    <nav>
+      <ul>
+        <li class="nav-item">
+          <img class="logo" alt="logo" src="./assets/build-a-bot-logo.png">
+          Build a bot
+        </li>
+      </ul>
+    </nav>
+  </header>
+  <main>
+  <RobotBuilder/>
+</main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+/* eslint-disable */ 
+import RobotBuilder from './build/RobotBuilder.vue';
+//import HomePage from './home/HomePage.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    RobotBuilder,
   },
 };
 </script>
 
 <style>
+body{
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,5 +37,25 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+header {
+  background-color: #999;
+  width: 1084px;
+  margin: 0 auto;
+}
+ul {
+  padding: 3px;
+  display: flex;
+}
+.nav-item {
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #bbb;
+}
+.logo {
+  vertical-align: middle;
+  height: 30px;
 }
 </style>
